c092097a5179441eea7a5111661d089e2353a344
Benjamin Florin <benjamin.florin@gmail.com>
2019-10-10 18:01:43 -0400

    Make search work on any server

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App({searchBox, searchString}) {
     return (
         <div className="bento-results-page">
             <h1 className="sr-only sr-only-focusable" id="main-heading">Search the library</h1>
-            <form method="get" action="/search" className="bento-search-box">
+            <form method="get" action={window.location.pathname} className="bento-search-box">
                 {searchBox}
                 <input type="submit" value="Search"/>
             </form>
