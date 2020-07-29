27cdc211e4a028c9d3cbb9be070aaeedda5457e2
Benjamin Florin <benjamin.florin@gmail.com>
2020-07-29 14:55:22 -0400

    Add finding aid icon

--- src/slots/books/FindingAidLink.jsx
+++ src/slots/books/FindingAidLink.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
+import FindingAidIcon from './icon_archives.svg';
 
-/**
- * Link to a BC finding aid
- *
- * Icons are provided in styles by the host site, not by this package.
- *
- * @param url
- * @return {*}
- * @constructor
- */
 function FindingAidLink({url}) {
-    return <div className="hathi-trust-link">
-        <div className="finding-aid-link__icon">
-            <a href={url} target="_blank"  rel="noopener noreferrer" >
-                Finding aid
-                <i className="fa fa-external-link" aria-hidden="true"/>
-            </a>
-        </div>
+    return <div className="finding-aid-link">
+        <img src={FindingAidIcon} className="finding-aid-link__icon" alt=""/>
+        <a href={url} target="_blank" rel="noopener noreferrer">
+            Finding aid
+            <i className="fa fa-external-link" aria-hidden="true"/>
+        </a>
     </div>
+
 }
 
 export default FindingAidLink;
