4c4859433a8eddadd92599f04d8c852b0c81e73c
Benjamin Florin <benjamin.florin@gmail.com>
2023-02-02 11:39:13 -0500

    Fix broken display of bad availabilities

--- src/slots/availability/AvailabilityContainer.jsx
+++ src/slots/availability/AvailabilityContainer.jsx
@@ -11,14 +11,44 @@ import CheckHoldingsMessage from "./CheckHoldingsMessage";
  * @constructor
  */
 function AvailabilityContainer({item}) {
-    if (item.availability) {
+    if (item.availability && item.availability.otherAvailabilities) {
         return <Availability firstHolding={item.availability} inOtherLibraries={item.availability.otherAvailabilities}/>
-    } else if (item.available === true || item.holdings[0].availabilityStatus === 'check_holdings') {
+    } else if (item.available === true || isCheckHoldingsResult(item)) {
         // If we've gotten here, something is wrong.
         return <CheckHoldingsMessage item={item} />
     } else {
         return <NotAvailableMessage item={item}/>;
     }
+}
+
+/**
+ *  Is this a "check holdings" item?
+ *
+ *  Check holdings is a catch-all for strange availability data. There are
+ *  a number of ways for availability records to be broken, so let's
+ *  dedicate an entire function to sussing them out.
+ *
+ * @param item
+ * @returns {boolean} true if we should display check-holdings
+ */
+function isCheckHoldingsResult(item) {
+    if (! item.holdings) {
+        return true;
+    }
+
+    if (! Array.isArray(item.holdings)) {
+        return true;
+    }
+
+    if (item.holdings.length === 0) {
+        return true;
+    }
+
+    if (!item.holdings[0].availabilityStatus ) {
+        return true;
+    }
+
+    return item.holdings[0].availabilityStatus === 'check_holdings';
 }
 
 export default AvailabilityContainer;
