33bd14dcc87b310a22d6cec6e7ee834fad6c8d0b
Benjamin Florin <benjamin.florin@gmail.com>
2024-04-03 15:20:19 -0400

    Get tests working

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import AppContainer from "./AppContainer.jsx"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <div>
-        <h1>asdas</h1>
         <AppContainer />
     </div>,
 );
