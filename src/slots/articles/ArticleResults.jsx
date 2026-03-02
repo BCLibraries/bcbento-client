37a61820bafb4509a4a3543e7dde00aa0fc1f0ed
Benjamin Florin <benjamin.florin@gmail.com>
2026-03-02 14:17:13 -0500

    Add 'expand search' link to no-results article searches

--- src/slots/articles/ArticleResults.jsx
+++ src/slots/articles/ArticleResults.jsx
@@ -13,6 +13,9 @@ import {ArticlesQuery} from "./ArticlesQuery";
  * @constructor
  */
 function ArticleResults({searchString, client, handleFetch}) {
+    const encodedSearchString = encodeURIComponent(searchString);
+    const expandedSearchUrl = `https://bc.primo.exlibrisgroup.com/discovery/search?query=any,contains,${encodedSearchString}&tab=CentralIndex&search_scope=CentralIndex&vid=01BC_INST:bclib&pcAvailability=false`
+
     return (
         <ResultsBoxContainer
             client={client}
@@ -21,8 +24,22 @@ function ArticleResults({searchString, client, handleFetch}) {
             term={'articles'}
             query={new ArticlesQuery(searchString)}
             renderResult={doc => <ArticleResult article={doc} key={`article-${doc.id}`}/>}
+            noResultsContent={noResultContent(searchString)}
             handleFetch={handleFetch}
         />
+    );
+}
+
+function noResultContent(searchString) {
+    const encodedSearchString = encodeURIComponent(searchString);
+    const expandedSearchUrl = `https://bc.primo.exlibrisgroup.com/discovery/search?query=any,contains,${encodedSearchString}&tab=CentralIndex&search_scope=CentralIndex&vid=01BC_INST:bclib&pcAvailability=true`
+        return (
+        <div className="catalog-no-results-box">
+            <p>There are no articles matching your search. </p>
+            <p className="catalog-no-results-box__worldcat-advice">
+                <a href={expandedSearchUrl}>Expand your search beyond library collections.</a>
+            </p>
+        </div>
     );
 }
 
